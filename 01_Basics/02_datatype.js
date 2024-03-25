ba5ae968dcacf89692811a41ca1a5fd5b7208675
Rahul Chaudhry <93764838+therkchaudhary@users.noreply.github.com>
2024-03-25 17:21:56 +0000

    Datatype

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict"  //treat all JS code as newer version of Javascript.
+//alert(3+3) //we are using nodejs, not browser.
+
+let name = "Rahul" //string => ""
+let age = 19  //number 2 to power 53.
+let isLoggedIn = false //boolean => true/false.
+let state = null //null => standalone value.
+let mode; //undefined => value dosen't defined.
+//symbol => unique
+
+
+//object
+
+console.log(typeof "Rahul");
+console.log(typeof null); //output => object
+console.log(typeof undefined); //output=> undefined
+
+

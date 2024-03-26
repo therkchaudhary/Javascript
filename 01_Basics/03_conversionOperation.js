64be4a90b252ab2cde067d0d326adac55c4c3697
Rahul Chaudhry <93764838+therkchaudhary@users.noreply.github.com>
2024-03-26 16:43:57 +0000

    Conversion datatype

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -9,12 +9,12 @@ let score = "Rahul"
 
 
 
-console.log(typeof score);
+// console.log(typeof score);
 // console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 
 
@@ -28,7 +28,7 @@ console.log(valueInNumber)
 let isLoggedIn = "Rahul"
 
 let booleanIsLoogedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoogedIn);
+// console.log(booleanIsLoogedIn);
 
 // 1 => True
 // 0 => False
@@ -38,5 +38,35 @@ console.log(booleanIsLoogedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// **************************** Operations **************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-3);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Rahul"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter = 100
+// ++gameCounter;
+gameCounter++;
+console.log(gameCounter);

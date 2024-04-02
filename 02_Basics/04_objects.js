82421bccde6847f108f748eb5811b11763f9ce49
therkchaudhary <rkchaudhary9354@gmail.com>
2024-04-02 05:02:38 +0000

    Objects

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -40,11 +40,39 @@ const users = [
 
 
 // users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+//course.courseInstructor
+
+const{courseInstructor:instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+//API method 1
+// {
+
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// method 2
+[
+    {},
+    {},
+    {}
+]

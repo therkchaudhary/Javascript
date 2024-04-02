a9532cd0ce5636d5ce43890f96e3565235ba9702
therkchaudhary <rkchaudhary9354@gmail.com>
2024-04-02 21:32:30 +0530

    function

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -68,3 +68,6 @@ function returnSecondValue(getArray){
 
 // console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([200, 400, 500, 1000]));
+console.log(returnSecondValue([200, 400, 500, 1000]));
+
+

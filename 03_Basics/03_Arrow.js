91809de347ef8c8254f53243c57bd199f24e2eca
therkchaudhary <rkchaudhary9354@gmail.com>
2024-04-03 20:12:09 +0530

    Arrow

---
+++ 03_Basics/03_Arrow.js
@@ -0,0 +1,48 @@
+const user = {
+    username: "rahul",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this)
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "rahul"
+//     console.log(this.username)
+// }
+
+// chai()
+
+
+// const chai = function(){
+//     let username = "hitesh"
+//     console.log(this.username)
+// }
+
+
+const chai = () => {
+    let username = "hitesh"
+    console.log(this)
+}
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  (num1 + num2)
+const addTwo = (num1, num2) =>  ({username: "Rahul"})
+
+console.log(addTwo(3, 4))
+
+

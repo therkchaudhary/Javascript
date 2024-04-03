04864a80f0445127968cd1d3f4aad17a0ab1480a
therkchaudhary <rkchaudhary9354@gmail.com>
2024-04-03 17:39:25 +0530

    scope

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -5,9 +5,49 @@ if (true) {
     let a = 10
     const b = 20
     // var c = 30
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "Rahul"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one( )
+
+if (true){
+    const username = "Rahul"
+    if(username === "Rahul"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+// +++++++++++++++ intresting ++++++++++++++++++++
+
+console.log(addon(5))
+function addon(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
